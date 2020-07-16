9b8e5f05fd847aa60026470241e18b3ba0ccb6bf
alwinlau <61771143+alwinlau@users.noreply.github.com>
2020-07-16 11:22:29 -0700

    Add files via upload

---
+++ SongOrganizer.php
@@ -0,0 +1,96 @@
+<!DOCTYPE>
+<html>
+<head>
+    <title>Song Organizer</title>
+</head>
+<body>
+    <h1>Song Organizer</h1>
+    <?php
+        if (isset($_GET['action'])) {
+            if ((file_exists("songs.txt")) && (filesize("songs.txt") != 0)) {
+                $SongArray = file("songs.txt");
+                switch ($_GET['action']) {
+                    case 'Remove Duplicates':
+                        $SongArray = array_unique($SongArray);
+                        $SongArray = array_values($SongArray);
+                        break;
+                    case 'Sort Ascending':
+                        sort($SongArray);
+                        break;
+                    case 'Shuffle':
+                        shuffle($SongArray);
+                        break;
+                } // end switch statement
+                if (count($SongArray)>0) {
+                    $NewSongs = implode($SongArray);
+                    $SongStore = fopen("songs.txt", "wb");
+                    if ($SongStore === false)
+                        echo "There was an error updating the song file\n";
+                    else {
+                        fwrite($SongStore, $NewSongs);
+                        fclose($SongStore);
+                    }
+                }
+                else
+                    unlink("songs.txt");
+            } // end inner if
+        } // end outer if
+        if (isset($_POST['submit'])) {
+            $SongToAdd = stripslashes($_POST['SongName']) . "\n";
+            $ExistingSongs = array();
+            if (file_exists("songs.txt") && filesize("songs.txt") > 0) {
+                $ExistingSongs = file("songs.txt");
+                if (in_array($SongToAdd, $ExistingSongs)) {
+                    echo "<p>The song you entered already exists!<br />\n";
+                    echo "Your song was not added to the list.</p>";
+                }
+                else {
+                    if (!empty($SongToAdd)) {
+                        $ExistingSongs = fopen("songs.txt", "ab");
+                        fwrite($ExistingSongs, $SongToAdd);
+                        fclose($ExistingSongs);
+                        echo "Your song has been added to the list.\n";
+                    }
+                    else
+                        echo "<p>You must enter the name of a song!<br />\n";
+                }
+            }
+            else {
+                $SongFile = fopen("songs.txt", "ab");
+                if ($SongFile === false)
+                    echo "There was an error saving your message!\n";
+                else {
+                    fwrite($SongFile, $SongToAdd);
+                    fclose($SongFile);
+                    echo "Your song has been added to the list.\n";
+                }
+            }
+        }
+        if ((!file_exists("songs.txt")) || (filesize("songs.txt") == 0))
+            echo "<p>There are no songs in the list.</p>\n";
+        else {
+            $SongArray = file("songs.txt");
+            echo "<table border=\"1\" width=\"100%\" style=\"background-color:lightgray\">\n";
+            foreach ($SongArray as $Song) {
+                echo "<tr>\n";
+                echo "<td>" . htmlentities($Song) . "</td>";
+                echo "</tr>\n";
+            }
+            echo "</table>\n";
+        }
+    ?>
+
+    <p>
+    <a href="SongOrganizer.php?action=Sort%20Ascending">Sort Song List</a><br />
+    <a href="SongOrganizer.php?action=Remove%20Duplicates">Remove Duplicate Songs</a><br />
+    <a href="SongOrganizer.php?action=Shuffle">Randomize Song list</a><br />
+    </p>
+
+    <form action="SongOrganizer.php" method="post">
+    <p>Add a New Song</p>
+    <p>Song Name: <input type="text" name="SongName" /></p>
+    <p><input type="submit" name="submit" value="Add Song to List" />
+    <input type="reset" name="reset" value="Reset Song Name" /></p>
+    </form>
+</body>
+</html>
